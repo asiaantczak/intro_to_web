017b1e01384e5ada16fb76e9bbd34fcafc22ddd5
Asia Antczak <aasiaantczak@gmail.com>
2018-04-30 16:29:30 +0100

    add form to submit names

--- app.rb
+++ app.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
 
-get '/cat' do
+get '/random-cat' do
+  @random_name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
